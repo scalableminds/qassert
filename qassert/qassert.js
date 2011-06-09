d3d88b75265c44d91fec51057361723c64fbdb4c
gabor.czigola@gmail.com <gabor.czigola@gmail.com>
2011-06-09 11:23:43 +0200

    deepEquals completed. API finished. TODO: - documentation/comments in qassert/qassert.js - complete examples in test/index.html

--- qassert/qassert.js
+++ qassert/qassert.js
@@ -14,12 +14,18 @@
      */
     var options = {
             enabled: false,
-            log: console && $.isFunction(console.log) ? console.log : $.noop,
+            log: console && $.isFunction(console.log) ? $.proxy(console.log, console) : $.noop,
             callback: $.noop,
             message: "Assertion failed on ",
             ajax: null,
-            ignoreStackTop: 7 // IMPORTANT: for this to work, all execution paths must be the same length between API<->stack trace generation
+            ignoreStackTop: 6 // IMPORTANT: for this to work, all execution paths must be the same length between API<->stack trace generation
     };
+
+    /**
+     **
+     ** API BEGIN
+     **
+     **/
 
     /**
      * Setup QAssert. Enables assertions to take action.
@@ -201,6 +207,44 @@
     }
 
     /**
+     * Recursive equality assertion. If disabled, no-op.
+     * Uses deepEquals().
+     *
+     * @param value    actual value to compare
+     * @param expected expected value to compare to
+     * @param message  optional message
+     * @returns value
+     */
+    $.assertDeepEquals = function (actual, expected, message) {
+        if (options.enabled) {
+            assert(deepEquals(actual, expected), message, actual);
+        }
+        return actual;
+    }
+
+    /**
+     * Recursive unequality assertion. If disabled, no-op.
+     * Uses !deepEquals().
+     *
+     * @param value    actual value to compare
+     * @param expected expected value to compare to
+     * @param message  optional message
+     * @returns value
+     */
+    $.assertNotDeepEquals = function (actual, expected, message) {
+        if (options.enabled) {
+            assert(!deepEquals(actual, expected), message, actual);
+        }
+        return actual;
+    }
+
+    /**
+     **
+     ** API END
+     **
+     **/
+
+    /**
      * Base assertion handler.
      */
     function assert(value, message, originalValue) {
@@ -213,7 +257,7 @@
     /**
      * Handles a failed assertion.
      */
-    function fail (value, message) {
+    function fail(value, message) {
         var stacktrace = getStackTrace();
         var context = options.callback(value, message, stacktrace);
         logToConsole(value, message, stacktrace, context);
@@ -285,155 +329,178 @@
         return a == b;
     }
 
-
- /*
-
-  deepEqual: function(actual, expected, message) {
-    QUnit.push(QUnit.equiv(actual, expected), actual, expected, message);
-  },
-
-  notDeepEqual: function(actual, expected, message) {
-    QUnit.push(!QUnit.equiv(actual, expected), actual, expected, message);
-  },
+    /**
+     **
+     ** MICRO LIBRARIES
+     **
+     **/
 
 
-        // for string, boolean, number and null
-        function useStrictEquality(b, a) {
-            if (b instanceof a.constructor || a instanceof b.constructor) {
-                // to catch short annotaion VS 'new' annotation of a declaration
-                // e.g. var i = 1;
-                //      var j = new Number(1);
-                return a == b;
-            } else {
-                return a === b;
+    // Test for equality any JavaScript type.
+    // Discussions and reference: http://philrathe.com/articles/equiv
+    // Test suites: http://philrathe.com/tests/equiv
+    // Author: Philippe Rathé <prathe@gmail.com>
+    var deepEquals = function (){
+
+        var innerEquiv; // the real equiv function
+        var callers = []; // stack to decide between skip/abort functions
+        var parents = []; // stack to avoiding loops from circular referencing
+
+        // Call the o related callback with the given arguments.
+        function bindCallbacks(o, callbacks, args) {
+            var prop = getType(o);
+            if (prop) {
+                if (getType(callbacks[prop]) === "function") {
+                    return callbacks[prop].apply(callbacks, args);
+                } else {
+                    return callbacks[prop]; // or undefined
+                }
             }
         }
 
-        return {
-            "string": useStrictEquality,
-            "boolean": useStrictEquality,
-            "number": useStrictEquality,
-            "null": useStrictEquality,
-            "undefined": useStrictEquality,
+        var callbacks = function () {
 
-            "nan": function (b) {
-                return isNaN(b);
-            },
-
-            "date": function (b, a) {
-                return QUnit.objectType(b) === "date" && a.valueOf() === b.valueOf();
-            },
-
-            "regexp": function (b, a) {
-                return QUnit.objectType(b) === "regexp" &&
-                    a.source === b.source && // the regex itself
-                    a.global === b.global && // and its modifers (gmi) ...
-                    a.ignoreCase === b.ignoreCase &&
-                    a.multiline === b.multiline;
-            },
-
-            // - skip when the property is a method of an instance (OOP)
-            // - abort otherwise,
-            //   initial === would have catch identical references anyway
-            "function": function () {
-                var caller = callers[callers.length - 1];
-                return caller !== Object &&
-                        typeof caller !== "undefined";
-            },
-
-            "array": function (b, a) {
-                var i, j, loop;
-                var len;
-
-                // b could be an object literal here
-                if ( ! (QUnit.objectType(b) === "array")) {
-                    return false;
+            // for string, boolean, number and null
+            function useStrictEquality(b, a) {
+                if (b instanceof a.constructor || a instanceof b.constructor) {
+                    // to catch short annotaion VS 'new' annotation of a declaration
+                    // e.g. var i = 1;
+                    // var j = new Number(1);
+                    return a == b;
+                } else {
+                    return a === b;
                 }
+            }
 
-                len = a.length;
-                if (len !== b.length) { // safe and faster
-                    return false;
-                }
+            return {
+                "string": useStrictEquality,
+                "boolean": useStrictEquality,
+                "number": useStrictEquality,
+                "null": useStrictEquality,
+                "undefined": useStrictEquality,
 
-                //track reference to avoid circular references
-                parents.push(a);
-                for (i = 0; i < len; i++) {
-                    loop = false;
-                    for(j=0;j<parents.length;j++){
-                        if(parents[j] === a[i]){
-                            loop = true;//dont rewalk array
-                        }
-                    }
-                    if (!loop && ! innerEquiv(a[i], b[i])) {
-                        parents.pop();
+                "nan": function (b) {
+                    return isNaN(b);
+                },
+
+                "date": function (b, a) {
+                    return getType(b) === "date" && a.valueOf() === b.valueOf();
+                },
+
+                "regexp": function (b, a) {
+                    return getType(b) === "regexp" &&
+                        a.source === b.source && // the regex itself
+                        a.global === b.global && // and its modifers (gmi) ...
+                        a.ignoreCase === b.ignoreCase &&
+                        a.multiline === b.multiline;
+                },
+
+                // - skip when the property is a method of an instance (OOP)
+                // - abort otherwise,
+                // initial === would have catch identical references anyway
+                "function": function () {
+                    var caller = callers[callers.length - 1];
+                    return caller !== Object &&
+                            typeof caller !== "undefined";
+                },
+
+                "array": function (b, a) {
+                    var i, j, loop;
+                    var len;
+
+                    // b could be an object literal here
+                    if ( ! (getType(b) === "array")) {
                         return false;
                     }
-                }
-                parents.pop();
-                return true;
-            },
 
-            "object": function (b, a) {
-                var i, j, loop;
-                var eq = true; // unless we can proove it
-                var aProperties = [], bProperties = []; // collection of strings
-
-                // comparing constructors is more strict than using instanceof
-                if ( a.constructor !== b.constructor) {
-                    return false;
-                }
-
-                // stack constructor before traversing properties
-                callers.push(a.constructor);
-                //track reference to avoid circular references
-                parents.push(a);
-
-                for (i in a) { // be strict: don't ensures hasOwnProperty and go deep
-                    loop = false;
-                    for(j=0;j<parents.length;j++){
-                        if(parents[j] === a[i])
-                            loop = true; //don't go down the same path twice
+                    len = a.length;
+                    if (len !== b.length) { // safe and faster
+                        return false;
                     }
-                    aProperties.push(i); // collect a's properties
 
-                    if (!loop && ! innerEquiv(a[i], b[i])) {
-                        eq = false;
-                        break;
+                    //track reference to avoid circular references
+                    parents.push(a);
+                    for (i = 0; i < len; i++) {
+                        loop = false;
+                        for(j=0;j<parents.length;j++){
+                            if(parents[j] === a[i]){
+                                loop = true;//dont rewalk array
+                            }
+                        }
+                        if (!loop && ! innerEquiv(a[i], b[i])) {
+                            parents.pop();
+                            return false;
+                        }
                     }
+                    parents.pop();
+                    return true;
+                },
+
+                "object": function (b, a) {
+                    var i, j, loop;
+                    var eq = true; // unless we can proove it
+                    var aProperties = [], bProperties = []; // collection of strings
+
+                    // comparing constructors is more strict than using instanceof
+                    if ( a.constructor !== b.constructor) {
+                        return false;
+                    }
+
+                    // stack constructor before traversing properties
+                    callers.push(a.constructor);
+                    //track reference to avoid circular references
+                    parents.push(a);
+
+                    for (i in a) { // be strict: don't ensures hasOwnProperty and go deep
+                        loop = false;
+                        for(j=0;j<parents.length;j++){
+                            if(parents[j] === a[i])
+                                loop = true; //don't go down the same path twice
+                        }
+                        aProperties.push(i); // collect a's properties
+
+                        if (!loop && ! innerEquiv(a[i], b[i])) {
+                            eq = false;
+                            break;
+                        }
+                    }
+
+                    callers.pop(); // unstack, we are done
+                    parents.pop();
+
+                    for (i in b) {
+                        bProperties.push(i); // collect b's properties
+                    }
+
+                    // Ensures identical properties name
+                    return eq && innerEquiv(aProperties.sort(), bProperties.sort());
                 }
+            };
+        }();
 
-                callers.pop(); // unstack, we are done
-                parents.pop();
-
-                for (i in b) {
-                    bProperties.push(i); // collect b's properties
-                }
-
-                // Ensures identical properties name
-                return eq && innerEquiv(aProperties.sort(), bProperties.sort());
+        innerEquiv = function () { // can take multiple arguments
+            var args = Array.prototype.slice.apply(arguments);
+            if (args.length < 2) {
+                return true; // end transition
             }
+
+            return (function (a, b) {
+                if (a === b) {
+                    return true; // catch the most you can
+                } else if (a === null || b === null || typeof a === "undefined" || typeof b === "undefined" || getType(a) !== getType(b)) {
+                    return false; // don't lose time with error prone cases
+                } else {
+                    return bindCallbacks(a, callbacks, [b, a]);
+                }
+
+            // apply transition with (1..n) arguments
+            })(args[0], args[1]) && arguments.callee.apply(this, args.splice(1, args.length -1));
         };
+
+        return innerEquiv;
+
     }();
 
-    innerEquiv = function () { // can take multiple arguments
-        var args = Array.prototype.slice.apply(arguments);
-        if (args.length < 2) {
-            return true; // end transition
-        }
-
-        return (function (a, b) {
-            if (a === b) {
-                return true; // catch the most you can
-            } else if (a === null || b === null || typeof a === "undefined" || typeof b === "undefined" || QUnit.objectType(a) !== QUnit.objectType(b)) {
-                return false; // don't lose time with error prone cases
-            } else {
-                return bindCallbacks(a, callbacks, [b, a]);
-            }
-
-        // apply transition with (1..n) arguments
-        })(args[0], args[1]) && arguments.callee.apply(this, args.splice(1, args.length -1));
-    };
-*/
     /**
      * Type detector.
      *
@@ -488,59 +555,58 @@
     }
 
 
-/**	   STACKTRACE
- **
- **    Micro-library for getting stack traces in all web browsers .
- **    https://github.com/emwendelin/javascript-stacktrace
- **    To be updated frequently.
- **/
+    /**	   STACKTRACE
+     **
+     **    Micro-library for getting stack traces in all web browsers .
+     **    https://github.com/emwendelin/javascript-stacktrace
+     **    To be updated frequently.
+     **/
 
-
-// Domain Public by Eric Wendelin http://eriwen.com/ (2008)
-//                  Luke Smith http://lucassmith.name/ (2008)
-//                  Loic Dachary <loic@dachary.org> (2008)
-//                  Johan Euphrosine <proppy@aminche.com> (2008)
-//                  Øyvind Sean Kinsey http://kinsey.no/blog (2010)
-//                  Victor Homyakov <victor-homyakov@users.sourceforge.net> (2010)
-//
-// Information and discussions
-// http://jspoker.pokersource.info/skin/test-printstacktrace.html
-// http://eriwen.com/javascript/js-stack-trace/
-// http://eriwen.com/javascript/stacktrace-update/
-// http://pastie.org/253058
-//
-// guessFunctionNameFromLines comes from firebug
-//
-// Software License Agreement (BSD License)
-//
-// Copyright (c) 2007, Parakey Inc.
-// All rights reserved.
-//
-// Redistribution and use of this software in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-// * Redistributions of source code must retain the above
-//   copyright notice, this list of conditions and the
-//   following disclaimer.
-//
-// * Redistributions in binary form must reproduce the above
-//   copyright notice, this list of conditions and the
-//   following disclaimer in the documentation and/or other
-//   materials provided with the distribution.
-//
-// * Neither the name of Parakey Inc. nor the names of its
-//   contributors may be used to endorse or promote products
-//   derived from this software without specific prior
-//   written permission of Parakey Inc.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    // Domain Public by Eric Wendelin http://eriwen.com/ (2008)
+    //                  Luke Smith http://lucassmith.name/ (2008)
+    //                  Loic Dachary <loic@dachary.org> (2008)
+    //                  Johan Euphrosine <proppy@aminche.com> (2008)
+    //                  Øyvind Sean Kinsey http://kinsey.no/blog (2010)
+    //                  Victor Homyakov <victor-homyakov@users.sourceforge.net> (2010)
+    //
+    // Information and discussions
+    // http://jspoker.pokersource.info/skin/test-printstacktrace.html
+    // http://eriwen.com/javascript/js-stack-trace/
+    // http://eriwen.com/javascript/stacktrace-update/
+    // http://pastie.org/253058
+    //
+    // guessFunctionNameFromLines comes from firebug
+    //
+    // Software License Agreement (BSD License)
+    //
+    // Copyright (c) 2007, Parakey Inc.
+    // All rights reserved.
+    //
+    // Redistribution and use of this software in source and binary forms, with or without modification,
+    // are permitted provided that the following conditions are met:
+    //
+    // * Redistributions of source code must retain the above
+    //   copyright notice, this list of conditions and the
+    //   following disclaimer.
+    //
+    // * Redistributions in binary form must reproduce the above
+    //   copyright notice, this list of conditions and the
+    //   following disclaimer in the documentation and/or other
+    //   materials provided with the distribution.
+    //
+    // * Neither the name of Parakey Inc. nor the names of its
+    //   contributors may be used to endorse or promote products
+    //   derived from this software without specific prior
+    //   written permission of Parakey Inc.
+    //
+    // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+    // IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+    // FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+    // CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    // DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+    // IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+    // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
     /**
@@ -853,12 +919,11 @@
         }
     };
 
-
-/**
- **
- **    END OF STACKTRACE
- **
- **/
+    /**
+     **
+     **    END OF STACKTRACE
+     **
+     **/
 
 
 })(jQuery);
